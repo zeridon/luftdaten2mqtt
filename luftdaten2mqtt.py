1eede6be6ce1ad92a1e39db3933f37ff8ba21624
Vladimir Vitkov <vvitkov@gmail.com>
2021-01-10 15:07:15 +0200

    Implement homeassistant autodiscovery

--- luftdaten2mqtt.py
+++ luftdaten2mqtt.py
@@ -9,14 +9,11 @@ import os
 
 # TODO separate routes and logic
 
-HTTP_PORT = 8080
-MQTT_HOST = "192.168.1.1"
-MQTT_TOPIC = "luftdaten/"
-MQTT_USER = "user"
-MQTT_PASS = "pass"
-
-VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m3'
+# units of measurements
+VOLUME_MICROGRAMS_PER_CUBIC_METER = 'µg/m³'
 TEMP_CELSIUS = '°C'
+
+# sensor names
 SENSOR_TEMPERATURE = 'temperature'
 SENSOR_HUMIDITY = 'humidity'
 SENSOR_BME280_TEMPERATURE = 'BME280_temperature'
@@ -38,11 +35,9 @@ SENSOR_SPS30_P1 = 'SPS30_P1'
 SENSOR_PMS_P0 = 'PMS_P0'
 SENSOR_PMS_P1 = 'PMS_P1'
 SENSOR_PMS_P2 = 'PMS_P2'
-#SENSOR_SAMPLES = 'samples'
-#SENSOR_MIN_MICRO = 'min_micro'
-#SENSOR_MAX_MICRO = 'max_micro'
-#SENSOR_INTERVAL = 'interval'
 
+# map for sensors
+# SENSOR_NAME: [ Friendly Name, Unit of measurement, Device class ]
 SENSOR_TYPES = {
     SENSOR_TEMPERATURE: ['Temperature', TEMP_CELSIUS, 'temperature'],
     SENSOR_HUMIDITY: ['Humidity', '%', 'humidity'],
@@ -65,10 +60,19 @@ SENSOR_TYPES = {
     SENSOR_PMS_P0: ['PM1', VOLUME_MICROGRAMS_PER_CUBIC_METER, None],
     SENSOR_PMS_P1: ['PM10', VOLUME_MICROGRAMS_PER_CUBIC_METER, None],
     SENSOR_PMS_P2: ['PM2.5', VOLUME_MICROGRAMS_PER_CUBIC_METER, None],
-    #SENSOR_SAMPLES: ['Samples', '', None],
-    #SENSOR_MIN_MICRO: ['Min Micro', '', None],
-    #SENSOR_MAX_MICRO: ['Max Micro', '', None],
-    #SENSOR_INTERVAL: ['Update Interval', '', None],
+}
+
+# icons for sensors that have no class
+SENSOR_ICONS = {
+    SENSOR_PM1: ['mdi:thought-bubble'],
+    SENSOR_PM2: ['mdi:thought-bubble-outline'],
+    SENSOR_SPS30_P0: ['mdi:cloud'],
+    SENSOR_SPS30_P2: ['mdi:thought-bubble-outline'],
+    SENSOR_SPS30_P4: ['mdi:cloud-outline'],
+    SENSOR_SPS30_P1: ['mdi:thought-bubble'],
+    SENSOR_PMS_P0: ['mdi:cloud'],
+    SENSOR_PMS_P1: ['mdi:thought-bubble'],
+    SENSOR_PMS_P2: ['mdi:thought-bubble-outline'],
 }
 
 application = bottle.default_app()
@@ -79,22 +83,15 @@ def route_luftdaten_json2mqtt():
     # take the json and parse it
     json_req = bottle.request.json
     logging.debug("json req received %s", json_req)
-    #headers_string = [
-    #    '{}: {}'.format(h, bottle.request.headers.get(h)) for h in bottle.request.headers.keys()
-    #]
-    #print('URL={}, method={}\nheaders:\n{}'.format(bottle.request.url, bottle.request.method,'\n'.join(headers_string)))
 
     if bottle.request.headers.get('X-Mac-Id'):
-        publish(json_req, MQTT_TOPIC + bottle.request.headers.get('X-Mac-ID') + "/")
-    #now = str(datetime.datetime.now())
-    #publish({"stats/last_update": now,
-    #         "stats/interval": 120}, MQTT_TOPIC)
+        publish(json_req, MQTT_TOPIC + bottle.request.headers.get('X-Mac-Id'))
 
 def publish(json, topic_prefix):
-    #print(json)
-    #print(json["sensordatavalues"][0]["value_type"])
+    """ Publish to mqtt whatever we can/want"""
+
     if json["software_version"]:
-        t = topic_prefix + "firmware"
+        t = topic_prefix + "/firmware"
         val = json["software_version"]
         logging.debug("publishing to broker: '%s' '%s'", t, str(val))
         CLIENT.publish(topic=t, payload=val, retain=False)
@@ -102,18 +99,53 @@ def publish(json, topic_prefix):
     interval = 0
     for item in json["sensordatavalues"]:
         if str(item["value_type"]) == 'interval':
-            interval = item["value"]
+            interval = int(item["value"])/1000
 
     for item in json["sensordatavalues"]:
-        logging.debug("Got value for " + str(item["value_type"]))
-        try:
-            logging.debug(SENSOR_TYPES[str(item["value_type"])][0])
-        except:
-            pass
-        t = topic_prefix + str(item["value_type"])
-        val = item["value"]
-        logging.debug("publishing to broker: '%s' '%s'", t, str(val))
-        CLIENT.publish(topic=t, payload=val, retain=False)
+        # do not explode on unknown measurements
+        if str(item["value_type"]) in SENSOR_TYPES:
+            uniq_id = str(
+                "luftdaten-" +
+                str(topic_prefix).split('/')[-1] + "-" +
+                str(item["value_type"])
+            ).lower()
+            dev_name = "Luftdaten " + str(bottle.request.headers.get('X-Sensor').split('-')[-1])
+
+            # homeassistant autodiscovery
+            t = "homeassistant/sensor/" + uniq_id + "/config"
+            val = {
+                "~": topic_prefix,
+                "name": dev_name + " " + str(SENSOR_TYPES[str(item["value_type"])][0]),
+                "stat_t": "~/"+str(item["value_type"]),
+                "frc_upd": "False",
+                "qos": 0,
+                "uniq_id": uniq_id,
+                "unit_of_meas": SENSOR_TYPES[str(item["value_type"])][1],
+                "dev": {
+                    "ids": [str(topic_prefix).split('/')[-1].lower()],
+                    "name": dev_name,
+                    "mdl": "DIY " + str(bottle.request.headers.get('X-Sensor')),
+                    "sw": json["software_version"],
+                    "mf": "DIY Luftdaten"
+                },
+                "exp_aft": 1.2*interval
+            }
+
+            # set device class if available, else set an icon to be better
+            # looking
+            if SENSOR_TYPES[str(item["value_type"])][2]:
+                payload["dev_cla"] = SENSOR_TYPES[str(item["value_type"])][2]
+            else:
+                payload["icon"] = SENSOR_ICONS[str(item["value_type"])][0]
+
+            logging.debug("publishing to broker: '%s' '%s'",t , str(val))
+            CLIENT.publish(topic=topic, payload=str(payload).replace("'", '"'), retain=False)
+
+            # report data
+            t = topic_prefix + "/" + str(item["value_type"])
+            val = item["value"]
+            logging.debug("publishing to broker: '%s' '%s'", t, str(val))
+            CLIENT.publish(topic=t, payload=val, retain=False)
 
 @application.get("/luftdaten")
 def route_index():
@@ -148,10 +180,11 @@ def setup():
 def run_server():
     """Run the bottle-server on the configured http-port."""
     bottle.run(
-            app=application,
-            host="0.0.0.0",
-            port=HTTP_PORT,
-            reloader=True)
+        app=application,
+        host="0.0.0.0",
+        port=HTTP_PORT,
+        reloader=True
+    )
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
